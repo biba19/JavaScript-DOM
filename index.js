bcc5be2ad61b53b06afaf71b51a93ef398e90a35
sarra <sarra.mehamel@yahoo.com>
2023-01-11 10:50:10 +0100

    Les éléments du DOM
    get element by : tag, classe name  and Id

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log(document.title);
 document.title = "Ce titre est beaucoup trop long";
+var tagName = document.getElementsByTagName("h2");
+console.log(tagName);
+var classeName = document.getElementsByClassName("text-red");
+console.log(classeName);
+var byId = document.getElementById("sous-titre-DOM");
+console.log(byId);
